af426f45d93e9a50cc457800ad31723817a7ea08
ceanna93 <fairyanna@naver.com>
2021-01-28 01:14:07 +0900

    Update solution_anna.py

--- coding_test/leet_09_PalindromeNumber/solution_anna.py
+++ coding_test/leet_09_PalindromeNumber/solution_anna.py
@@ -1,7 +1,20 @@
 class Solution:
+    ##### STRING
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return 0
         if str(x) == str(x)[::-1]:
             return 1
         return 0
+
+    ##### MATH
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return 0
+        ret = 0
+        d = x
+        while d:
+            ret *= 10
+            ret += (d % 10)
+            d //= 10
+        return (ret == x)
